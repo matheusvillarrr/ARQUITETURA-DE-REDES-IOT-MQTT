0e0b5cb3f49eb121198041673f884d6b861f8cfa
matheusvillarrr <144268835+matheusvillarrr@users.noreply.github.com>
2025-02-13 07:54:35 -0300

    Add files via upload

---
+++ publisher.py
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+
+broker = "broker.hivemq.com"  
+port = 1883
+topic = "senai/dev"
+
+client = mqtt.Client()
+client.connect(broker, port)
+
+while True:
+    mensagem = input("Digite uma mensagem para enviar: ")
+    client.publish(topic, mensagem)
+    print(f"Mensagem '{mensagem}' publicada no tópico '{topic}'")
